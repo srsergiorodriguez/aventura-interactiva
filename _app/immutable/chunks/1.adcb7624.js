7269107ed9e1141ef3109f300d04b28baf0da9af
Sergio Rodríguez Gómez <46205341+srsergiorodriguez@users.noreply.github.com>
2023-10-24 12:10:11 -0500

    Updates

---
+++ _app/immutable/chunks/1.adcb7624.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.ddd34dbb.js";export{t as component};
--- _app/immutable/chunks/1.f5affe12.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.da722fde.js";export{t as component};
